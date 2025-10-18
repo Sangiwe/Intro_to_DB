cb9e9e97b5204fa06ff5a308a641b84162b36b50
Sangiwe <nokubongasangiwe@gmail.com>
2025-10-18 11:39:21 +0200

    Added MYSQLServer

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,31 +1,36 @@
 #!/usr/bin/python3
 """
-A simple script to create the 'alx_book_store' database in MySQL using PyMySQL.
+A simple script to create the 'alx_book_store' database in MySQL using mysql.connector.
 """
 
-import pymysql
+import mysql.connector
+from mysql.connector import Error
 
 def create_database():
     connection = None
     try:
         # Connect to MySQL Server
-        connection = pymysql.connect(
+        connection = mysql.connector.connect(
             host="localhost",
             user="root",             # change if needed
             password="@sql-Software-0745464042" # replace with your MySQL password
         )
 
-        with connection.cursor() as cursor:
+        if connection.is_connected():
+            cursor = connection.cursor()
             # Create the database if it doesn't exist
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except Exception as e:
+    except Error as e:
         print(f"Error while connecting to MySQL: {e}")
 
     finally:
-        if connection:
+        if connection and connection.is_connected():
+            cursor.close()
             connection.close()
 
 if __name__ == "__main__":
     create_database()
+
+
